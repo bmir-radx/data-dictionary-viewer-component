9dd9601855a7082f127457eeb90b692ff5d57590
cyouh95 <cyouh95@g.ucla.edu>
2025-07-16 01:06:12 -0700

    test github pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,35 @@ import App from './components/App/App';
 
 const rootEl = document.getElementById('root');
 
-const config = {
-  defaultView: rootEl.dataset.defaultView || 'list',
-  searchField: rootEl.dataset.searchField !== 'false',
-  dataDictionaryTitle: rootEl.dataset.dataDictionaryTitle !== 'false',
-};
-
 const root = createRoot(rootEl);
 
-root.render(
-  <StrictMode>
-    <App config={config} />
-  </StrictMode>
-);
+document.getElementById('fileInput').addEventListener('change', event => {
+    const fileList = event.target.files;
+    if (fileList.length > 0) {
+        const reader = new FileReader();
+        reader.onload = e => {
+            root.render(
+                <StrictMode>
+                    <App data={e.target.result} />
+                </StrictMode>
+            );
+        };
+        reader.readAsText(fileList[0]);
+    }
+});
+
+document.querySelector('#quote input[type="submit"]').addEventListener('click', () => {
+    root.render(
+        <StrictMode>
+            <App data={document.querySelector('#quote textarea').value} />
+        </StrictMode>
+    );
+});
+
+document.querySelector('#link input[type="submit"]').addEventListener('click', () => {
+    root.render(
+        <StrictMode>
+            <App data={`https://radxdatahub.nih.gov/api/launch/StudyOverview/getMetadata?fileId=${document.querySelector('#link input[type="text"]').value}`} />
+        </StrictMode>
+    );
+});
