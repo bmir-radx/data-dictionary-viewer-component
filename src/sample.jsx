60dfedebb52e39108281a4e2f92dfcebd3054b0c
cyouh95 <cyouh95@g.ucla.edu>
2025-08-26 17:09:00 -0700

    add demo

---
+++ src/sample.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './components/App/App';
+
+const rootEl = document.getElementById('root');
+
+const root = createRoot(rootEl);
+
+root.render(
+    <StrictMode>
+        <App data='/sample.csv' initialView='table' />
+    </StrictMode>
+);
