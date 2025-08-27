c4598698a3ad844f2a1a47d8abe473127ebe29ee
cyouh95 <cyouh95@g.ucla.edu>
2025-08-26 17:17:12 -0700

    update demo

--- src/main.jsx
+++ src/main.jsx
@@ -7,33 +7,8 @@ const rootEl = document.getElementById('root');
 
 const root = createRoot(rootEl);
 
-document.getElementById('fileInput').addEventListener('change', event => {
-    const fileList = event.target.files;
-    if (fileList.length > 0) {
-        const reader = new FileReader();
-        reader.onload = e => {
-            root.render(
-                <StrictMode>
-                    <App data={e.target.result} />
-                </StrictMode>
-            );
-        };
-        reader.readAsText(fileList[0]);
-    }
-});
-
-document.querySelector('#quote input[type="submit"]').addEventListener('click', () => {
-    root.render(
-        <StrictMode>
-            <App data={document.querySelector('#quote textarea').value} />
-        </StrictMode>
-    );
-});
-
-document.querySelector('#link input[type="submit"]').addEventListener('click', () => {
-    root.render(
-        <StrictMode>
-            <App data={`https://radxdatahub.nih.gov/api/launch/StudyOverview/getMetadata?fileId=${document.querySelector('#link input[type="text"]').value}`} />
-        </StrictMode>
-    );
-});
+root.render(
+    <StrictMode>
+        <App data='/sample.csv' initialView='table' />
+    </StrictMode>
+);
